276c6fe7eb6a0bfa8c2d639fc7ee0f5c994c36be
Jamie Vullo <jvullo@protonmail.com>
2019-04-23 14:07:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  new_array = []
+  
+  i = 0 
+  while i < array.length 
+    x = array[i].upcase
+    new_array << x 
 
+    i += 1 
+  end
+  
+  return new_array
+end
 
+#{|capitals|array[i].split(" ").first}
